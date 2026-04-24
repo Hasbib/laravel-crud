292b7e86f5105055a553e26677af2e1797bc94b4
Hasbib <shohiburrotibhabib@gmail.com>
2026-04-24 18:25:11 +0700

    PUSH - [auth]

--- database/migrations/2025_06_21_024933_app_table.php
+++ database/migrations/2025_06_21_024933_app_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Table Users
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name');
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
